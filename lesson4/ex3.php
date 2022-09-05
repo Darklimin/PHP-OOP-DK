3e1145d7ab4bf58c20edb9b50b89a7623f961520
Darius Kliminskas <dariuskliminskas@gmail.com>
2022-09-05 20:13:06 +0300

    Ex3 all tasks completed

---
+++ lesson4/ex3.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+class Person
+{
+    public string $name;
+    public string $surname;
+
+    public function __construct(string $name, string $surname)
+    {
+        $this->name = $name;
+        $this->surname = $surname;
+    }
+
+    public function __toString (): string {
+        return 'This person is called ' . $this->name . ' ' . $this->surname;
+    }
+
+}
+
+/*
+Paredaguokite klasę Person, kad veiktų šis kodas:
+$person = new Person('John', 'Smith');
+echo $person; // "This person is called John Smith"
+*/
+
+$person = new Person('John', 'Smith');
+echo $person; // "This person is called John Smith"
